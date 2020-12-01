27b01064c104f2be86926722a8e8a58c86c37c5a
H2WO4 <54752489+H2WO4@users.noreply.github.com>
2020-12-01 10:11:01 +0100

    Update

--- CustomCardMultiPreview.java
+++ CustomCardMultiPreview.java
@@ -1,4 +1,4 @@
-// package yourModID.cards;
+package BoosterPacks.cards;
 import basemod.abstracts.CustomCard;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -9,8 +9,12 @@ import java.util.ArrayList;
 
 public abstract class CustomCardMultiPreview extends CustomCard {
 
-    public ArrayList<AbstractCard> cardToPreview = new ArrayList<AbstractCard>();
+    public ArrayList<AbstractCard> cardToPreview = new ArrayList<>();
     private boolean renderTip;
+    private final int[] xSingle = new int[]{0, -280, -280};
+    private final int[] ySingle = new int[]{0, 0, -360};
+    private final int[] xMulti = new int[]{0, 0, 180};
+    private final int[] yMulti = new int[]{0, 250, 250};
 
     public CustomCardMultiPreview(final String id,
                                   final String name,
@@ -28,28 +32,26 @@ public abstract class CustomCardMultiPreview extends CustomCard {
 
     @Override
     public void renderCardPreviewInSingleView(SpriteBatch sb) {
-        float size = 1.1F - (0.1F * cardToPreview.size());
         for (int i = 0; i < cardToPreview.size(); i++) {
-            this.cardToPreview.get(i).current_x = 485.0F * Settings.scale;
-            this.cardToPreview.get(i).current_y = 795.0F - (360.0F * i * size) * Settings.scale;
-            this.cardToPreview.get(i).drawScale = 0.8F * size;
+            this.cardToPreview.get(i).current_x = (485.0F + this.xSingle[i]) * Settings.scale;
+            this.cardToPreview.get(i).current_y = (795.0F + this.ySingle[i]) * Settings.scale;
+            this.cardToPreview.get(i).drawScale = 0.8F;
             this.cardToPreview.get(i).render(sb);
         }
     }
 
     @Override
     public void renderCardPreview(SpriteBatch sb) {
-        float size = 1.1F - (0.1F * cardToPreview.size());
         for (int i = 0; i < cardToPreview.size(); i++) {
             if (AbstractDungeon.player == null || !AbstractDungeon.player.isDraggingCard) {
-                float tmpScale = this.drawScale * size * 0.8F;
+                float tmpScale = this.drawScale * 0.8F;
                 if (this.current_x > (float)Settings.WIDTH * 0.75F) {
-                    this.cardToPreview.get(i).current_x = this.current_x + (IMG_WIDTH / 2.0F + IMG_WIDTH / 2.0F * 0.8F + 16.0F) * this.drawScale;
+                    this.cardToPreview.get(i).current_x = (this.current_x + (IMG_WIDTH / 2.0F + IMG_WIDTH / 2.0F * 0.8F + 16.0F) - this.xMulti[i]) * this.drawScale;
                 } else {
-                    this.cardToPreview.get(i).current_x = this.current_x - (IMG_WIDTH / 2.0F + IMG_WIDTH / 2.0F * 0.8F + 16.0F) * this.drawScale;
+                    this.cardToPreview.get(i).current_x = (this.current_x - (IMG_WIDTH / 2.0F + IMG_WIDTH / 2.0F * 0.8F + 16.0F) + this.xMulti[i]) * this.drawScale;
                 }
-                this.cardToPreview.get(i).current_y = this.current_y + (IMG_HEIGHT / 2.0F - IMG_HEIGHT / 2.0F * 0.8F) + (360.0F * i * size) * this.drawScale;
-                this.cardToPreview.get(i).drawScale = tmpScale * size;
+                this.cardToPreview.get(i).current_y = (this.current_y + (IMG_HEIGHT / 2.0F - IMG_HEIGHT / 2.0F * 0.8F) + this.yMulti[i]) * this.drawScale;
+                this.cardToPreview.get(i).drawScale = tmpScale;
                 this.cardToPreview.get(i).render(sb);
             }
         }
