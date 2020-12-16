932787babf60f4eb9a395ba4d07dcd68192ec8f6
H2WO4 <54752489+H2WO4@users.noreply.github.com>
2020-12-16 10:55:15 +0100

    Update ExampleCard

--- ExampleCard.java
+++ ExampleCard.java
@@ -9,51 +9,51 @@ import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.status.Dazed;
 import com.megacrit.cardcrawl.cards.status.Wound;
+import com.megacrit.cardcrawl.cards.status.Slimed;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-import static BoosterPacks.BoosterPacks.makeCardPath;
+import static yourMod.yourMod.makeCardPath;
 
-public class Ravage extends CustomCardMultiPreview {
+public class ExampleCard extends CustomCardMultiPreview {
 
-    public static final String ID = BoosterPacks.makeID(Ravage.class.getSimpleName());
+    public static final String ID = yourMod.makeID(ExampleCard.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("Skill.png");
 
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
-    private static final CardRarity RARITY = CardRarity.SPECIAL;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
-    private static final CardType TYPE = CardType.ATTACK;
-    public static final CardColor COLOR = CardColor.COLORLESS;
-    private static final int COST = 1;
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.NONE;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = CardColor.RED;
+    private static final int COST = 0;
 
-    public Ravage() {
+    public ExampleCard() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        this.baseDamage = 16;
-        this.damage = this.baseDamage;
-        this.exhaust = true;
-        this.selfRetain = true;
         this.cardToPreview.add(new Wound());
         this.cardToPreview.add(new Dazed());
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
-        this.addToBot(new MakeTempCardInDrawPileAction(new Wound(), 1, true, false));
-        this.addToBot(new MakeTempCardInDrawPileAction(new Dazed(), 1, true, false));
+        this.addToBot(new MakeTempCardInHandAction(new Wound(), 1));
+        this.addToBot(new MakeTempCardInHandAction(new Dazed(), 1));
+        if (this.upgraded) {
+            this.addToBot(new MakeTempCardInHandAction(new Slimed(), 1))
+        }
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeDamage(6);
+            this.cardToPreview.add(new Slimed())
+            this.rawDescription = cardStrings.UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
